bb87565f4a28fe42dc13f8e64f8a14da5fc28f90
Jack S. Hale <mail@jackhale.co.uk>
2025-05-16 09:01:01 +0200

    Added test suite

--- test_add.py
+++ test_add.py
@@ -1,4 +1,15 @@
+import pytest
 from add import add
+import time
 
-def test_add():
-    assert add(3, 9) == 12
+@pytest.mark.parametrize("a,b,result", ((3, 9, 12), (-4, -8, -12), (-20, -40, -60)))
+def test_add(a, b, result):
+    assert add(a, b) == result
+
+def test_failure_on_string():
+    with pytest.raises(TypeError):
+        c = add(4, "b")
+
+@pytest.mark.slow
+def test_slow_test():
+   time.sleep(5) 
